7f9e2253d8eb463f18abfdc1921e9310aedd3df4
Clement Thiebault <clement.thiebault@dolphin.fr>
2024-06-19 11:54:19 -0400

    add tree now need fix

--- Forgelance.py
+++ Forgelance.py
@@ -4,36 +4,43 @@ import numpy as np
 # Les dégats de Lance incendiaire sont augmenté par les autres sorts de la voie, et sont réinitialisé une fois que le sort à proc
 # La variable "Lance" correspond à cet état, si Lance = False, l'ennemi n'a pas l'état
 
+
+# =============
+# CONST ZONE
+# =============
+
 # Nombre d'ennemis*4 que vous touchez avec les sorts de buff de la forgelance (pour chaque ennemis touché, Lanceincendiaire a ses dégats de bases augmentés de 4 jusqu'au prochain proc) 
 BUFFLANCEINCENDIAIRE = 4
+MINSPELLCOST = 2
+PA = 12
+Lance = False
+BuffLanceIncendiaireTotal = 0
 
-class Spell:
-    #Le coup en PA pour savoir si on continue
-    #L'id du sort
-    #Les dmg
-    #Les enfants
-    def __init__(self, dmg, cost, name):
+
+# =============
+
+class Step:
+    def __init__(self, dmg, name,paRem,lancerTrack):
         self.name = name
         self.dmg = dmg
-        self.cost = cost
+        self.paRem = paRem
         self.children = []
+        self.lancerTrack = lancerTrack
 
     def add_child(self, child_node):
         self.children.append(child_node)
 
+class Racine:
+    def __init__(self, name,paRem,lancerTrack):
+        self.name = name
+        self.paRem = paRem
+        self.lancerTrack = lancerTrack
 
-class Tour:
-    def __init__(self, root):
-        self.root = root
-
-def print_tree(node, level=0):
-    print(' ' * level+ node.name)
+def print_tree(node, currDmg, level=0):
+    print(' ' * level * 2 + node.name +" : " + str(currDmg))
     for child in node.children:
-        print_tree(child, level + 1)
+        print_tree(child,currDmg+node.dmg,level + 1)
 
-
-def max_value(inputlist):
-    return max([sublist[-1] for sublist in inputlist])
 
 # Definition des différents sorts de la Forgelance
 
@@ -48,173 +55,99 @@ def LanceIncendiaire():
 
 #ça pourrait être moins lourd ? de ne passer que la moyenne en param et pas une liste, mais beaucoup moins lisible
 
-def apply_spell(pa_cost,dmg_range,buffCoef=1,Muspel=False):
-    global Lance, DSorts, BuffLanceIncendiaireTotal, PA
-    if pa_cost >= PA:
-        #Add damage
-        DSorts += np.average(dmg_range)
+def apply_spell(dmg_range,buffCoef=1,Muspel=False):
+    global Lance, BuffLanceIncendiaireTotal
+    Lance = Lance
+    #On sait déjà que si on est la c'est qu'on a les PA
+    currDmg = 0
+    #Add damage
+    currDmg += np.average(dmg_range)
+    
+    #On proc que si on est à buff = 0
+    if buffCoef == 0 and Lance:
+        currDmg+=LanceIncendiaire()
+    elif not Lance and not Muspel:
+        Lance = True
+    
+    #Buff the lanceBuff if needed, sometime it can be 0 but anyway
+    BuffLanceIncendiaireTotal += BUFFLANCEINCENDIAIRE*buffCoef
+    return currDmg
         
-        #On proc que si on est à buff = 0
-        if buffCoef == 0 and Lance:
-            DSorts+=LanceIncendiaire()
-        elif not Lance and not Muspel:
-            Lance = True
-        
-        #Buff the lanceBuff if needed, sometime it can be 0 but anyway
-        BuffLanceIncendiaireTotal += BUFFLANCEINCENDIAIRE*buffCoef
-        #Sub PA cost
-        PA -= pa_cost
-        
+#0
 def LanceAIncendie(): #proc
-    apply_spell(3,[23,26],0)
+    return apply_spell([23,26],0)
     
 def MoulinRouge(): #B1
-   apply_spell(3,[28,32])
-   #I don't like returning nothing but if we keep this struct...
-
+    return apply_spell([28,32])
+  
 def Fente(): #B1
-    apply_spell(2,[12,14])
+    return apply_spell([12,14])
 
 def FerRouge(): #B0.5
-    apply_spell(3,[27,30],0.5)
+    return apply_spell([27,30],0.5)
 
 def EstocBrulant():#B1
-    apply_spell(3,[25,28])
+    return apply_spell([25,28])
 
 #Muspel proc mais n'applique pas
 def Muspel():
-    apply_spell(3,[31,35],0,Muspel=True)
+    return apply_spell([31,35],0,Muspel=True)
 
-# Sort Numéro 6
+#6
 def Maelstom(): #Proc
-    apply_spell(3,[16,19],0)
+    return apply_spell([16,19],0)
 
 
 # =============
 # Main
 # =============
-Tour1 = []
-TourFiltre = []
 
-# Liste de fonctions correspondantes aux sorts feu de la Forgelance
-Sorts = (LanceAIncendie,MoulinRouge,Fente,FerRouge,EstocBrulant,Muspel,Maelstom)
-
-
-# 4 Boucle pour itérer à travers les sorts et tester toutes les combinaison possibles
-# Problèmes majeurs: 
-# Besoin de "sauvegarder" les Pa, Dégat, les Buff et l'état de la Lance à un moment donné
-# Besoin de faire une boucle par sorts lancé, serait plus pratique si ça détectait, avec les PA restants, s'il est possible de lancer un sort (utile si on se fait Re Pa)
+SPELLREQ = {0 :{"id" : 0, "fct" :LanceAIncendie, "name" : "LanceAIncendie","pa" : 3 ,"max" : 3 },\
+            1 :{"id" : 1, "fct" : MoulinRouge, "name" : "MoulinRouge","pa" : 3,"max" :1},\
+            2 :{"id" : 2, "fct" : Fente, "name" : "Fente","pa" : 2,"max" :1},\
+            3 :{"id" : 3, "fct" : FerRouge, "name" : "FerRouge","pa" : 3,"max" :2},\
+            4 :{"id" : 4, "fct" : EstocBrulant, "name" : "EstocBrulant","pa" : 3,"max" :2},\
+            5 :{"id" : 5, "fct" : Muspel, "name" : "Muspel","pa" : 4,"max" :1},\
+            6 :{"id" : 6, "fct" : Maelstom, "name" : "Maelstom","pa" : 3,"max" :2},\
+            }
 
 
 
-# Nouveau format de sortie:
-
-# Une grande liste
-
-for i in range(0,len(Sorts)):
-
-    PA = 12
-    BuffLanceIncendiaireTotal = 0
-    Lance = False
-    DSorts = 0
-
-    Sorts[i]()
-
-    PAE1 = PA
-    DSortsE1 = DSorts
-    BuffLanceIncendiaireTotalE1 = BuffLanceIncendiaireTotal
-    LanceE1 = Lance
+def stillLaunch(currStep):
+    if currStep.paRem < MINSPELLCOST:
+        return False
+    #Pas besoin de regarder les cumuls max
+    #ça aurait été une condition d'arrêt s'il était possible de lancer tout les sorts avec encore des PA
+    return True
     
-    for j in range(0,len(Sorts)):
+def buildTree(currStep):
+    #On regarde quel(s) sorts on peut lancer PA/lancer max
+    toBeLaunch = []
+    for key,value in SPELLREQ.items():
+        if currStep.paRem >= value["pa"] and currStep.lancerTrack[key] < value["max"]:
+            toBeLaunch.append(value)
+    #On lance les sorts possibles
+    for spell in toBeLaunch:
+        #On a lancé un sort, du coup on le prépare et on ajoute
+        thisSpell = spell
+        spellDmg = thisSpell["fct"]()
+        name = thisSpell["name"]
+        paRem = currStep.paRem - thisSpell["pa"]
+        lancerTrack = currStep.lancerTrack
+        lancerTrack[thisSpell["id"]]+=1
+        thisStep = Step(spellDmg,name,paRem,lancerTrack)
+        #On ajoute ce choix au noeud actuel
+        currStep.add_child(thisStep)
+        
+        #On regarde si on pourra en lancer au prochain tour à partir de ce step la
+        #(Il est vrai que même si seul un est possible on fait un for/if sur 7 index mais blc)
+        if stillLaunch(thisStep):
+            buildTree(thisStep) #build tree from this step
+        
+    
 
-        PA = PAE1
-        BuffLanceIncendiaireTotal = BuffLanceIncendiaireTotalE1
-        Lance = LanceE1
-        DSorts = DSortsE1
-
-        Sorts[j]()
-
-        PAE2 = PA
-        DSortsE2 = DSorts
-        BuffLanceIncendiaireTotalE2 = BuffLanceIncendiaireTotal
-        LanceE2 = Lance
-
-        for k in range(0,len(Sorts)):
-
-            PA = PAE2
-            BuffLanceIncendiaireTotal = BuffLanceIncendiaireTotalE2
-            Lance = LanceE2
-            DSorts = DSortsE2
-
-            Sorts[k]()
-
-            PAE3 = PA
-            DSortsE3 = DSorts
-            BuffLanceIncendiaireTotalE3 = BuffLanceIncendiaireTotal
-            LanceE3 = Lance
-
-            if PA>0:
-                for l in range(0,len(Sorts)):
-
-                    PA = PAE3
-                    BuffLanceIncendiaireTotal = BuffLanceIncendiaireTotalE3
-                    Lance = LanceE3
-                    DSorts = DSortsE3
-                    
-                    Sorts[l]()
-
-                    if PA>=0:
-                        Tour1.append((i,j,k,l,DSorts))
-                    else:
-                        Tour1.append((i,j,k,DSortsE3))
-
-            elif PA == 0:
-
-                Tour1.append((i,j,k,DSorts))
-
-# Sert a retirer les séquences de sorts où un sort a plus d'utilisation que sa limite par tour
-for sublist in Tour1:
-    NBLAI = sublist.count(0)
-    NBMORO = sublist.count(1)
-    NBFENT = sublist.count(2)
-    NBFERO = sublist.count(3)
-    NBESTC = sublist.count(4)
-    NBMUS = sublist.count(5)
-    NBMAEL = sublist.count(6)
-    if NBLAI < 3 and NBFERO < 3 and NBESTC < 3 and NBMAEL < 3 and NBMORO < 2 and NBFENT < 2 and NBMUS < 2:
-        TourFiltre.append(sublist)
-
-# Détermine le maximum de dégats de sorts dans les conditions données
-GrosDégat = max_value(TourFiltre)
-
-# Print les séquences obtenant ce résultat
-for sublist in TourFiltre:
-    if GrosDégat in sublist:
-        print(sublist)
-
-
-root = Spell("Root")
-child_a = Spell("A")
-child_b = Spell("B")
-child_a1 = Spell("A1")
-child_a2 = Spell("A2")
-child_b1 = Spell("B1")
-child_a1_1 = Spell("A1.1")
-child_a1_2 = Spell("A1.2")
-child_a2_1 = Spell("A2.1")
-child_b1_1 = Spell("B1.1")
-
-root.add_child(child_a)
-root.add_child(child_b)
-child_a.add_child(child_a1)
-child_a.add_child(child_a2)
-child_b.add_child(child_b1)
-child_a1.add_child(child_a1_1)
-child_a1.add_child(child_a1_2)
-child_a2.add_child(child_a2_1)
-child_b1.add_child(child_b1_1)
-
-tree = Tour(root)
-
-print_tree(tree.root)
+#Il faudra juste compare les plus gros résulats
+strat = Step(0,"Start",PA,[0,0,0,0,0,0,0])
+buildTree(strat)
+print_tree(strat,0)
 
