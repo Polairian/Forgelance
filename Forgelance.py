dd251f203d0c60803be2150749d96c8739c13d59
Polairian <vutacle@gmail.com>
2024-06-20 11:16:01 +0200

    Ajout mécanique de stats, dégats et critique

--- Forgelance.py
+++ Forgelance.py
@@ -1,207 +1,76 @@
+
 import numpy as np
+import pandas as pd
 
-# La voie feu de la forgelance fonctionne autour du sort Lance Incendiaire, 3 des sorts permettent de poser et/ou retirer un état aux ennemis, lorsqu'on le retire, l'ennemi subit le sort Lance Incendiaire
-# Les dégats de Lance incendiaire sont augmenté par les autres sorts de la voie, et sont réinitialisé une fois que le sort à proc
-# La variable "Lance" correspond à cet état, si Lance = False, l'ennemi n'a pas l'état
+# =============
+# CONST ZONE
+# =============
 
-# Nombre d'ennemis*4 que vous touchez avec les sorts de buff de la forgelance (pour chaque ennemis touché, Lanceincendiaire a ses dégats de bases augmentés de 4 jusqu'au prochain proc) 
 BuffLanceIncendiaire = 4
+PA = 12
+equipmentCritChance = 84
+stats = pd.DataFrame([['-', 450, 450, 460, 480, 435, '-'], [48, 83, 74, 57, 109, '-', 138]], ['Characteristics','Do'], ['Neutre','Terre', 'Feu', 'Eau', 'Air','Puissance/Dommages', 'Crit'])
 
 
-def max_value(inputlist):
-    return max([sublist[-1] for sublist in inputlist])
+# =============
 
-# Definition des différents sorts de la Forgelance
+initial_state = (0, 0, 0, False)
+DSorts, Dégats, BuffLanceIncendiaireTotal, Lance = initial_state
+
 def LanceIncendiaire():
-    
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
-
+    global Lance, DSorts, BuffLanceIncendiaireTotal, Dégats, stats
     Lance = False
-    DSorts += BuffLanceIncendiaireTotal + (18+20)/2
+    DSorts += BuffLanceIncendiaireTotal + 19  # (18+20)/2
+    Dégats += (BuffLanceIncendiaireTotal + 19)* (stats.loc['Characteristics','Feu'] + stats.loc['Characteristics','Puissance/Dommages'])/ 100
     BuffLanceIncendiaireTotal = 0
-    return
 
-# Sort Numéro 0
-def LanceAIncendie():
-
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
-
-    if PA >= 3:
-        
-        DSorts += (23+26)/2
-        if Lance == False:
+def apply_sort(damage_range, critChance, Element, buff=0, proc=False, muspel=False):
+    global Lance, DSorts, BuffLanceIncendiaireTotal, equipmentCritChance, Dégats, stats
+    totalCritChance = critChance+equipmentCritChance if critChance+equipmentCritChance < 100 else 100
+    DSorts += np.average(damage_range[:2])* (1-(totalCritChance/100)) + np.average(damage_range[2:])* totalCritChance/100
+    Dégats += (np.average(damage_range[:2])* (1-(totalCritChance/100)) + np.average(damage_range[2:])* totalCritChance/100)* (stats.loc['Characteristics',Element] + stats.loc['Characteristics','Puissance/Dommages'])/ 100 +  stats.loc['Do',Element] + stats.loc['Do','Crit']* totalCritChance/ 100
+    BuffLanceIncendiaireTotal += buff
+    if proc:
+        if Lance:
+            LanceIncendiaire()
+        elif not muspel:
             Lance = True
-        elif Lance == True:
-            LanceIncendiaire()
-    PA -= 3
-    return
-    
-# Sort Numéro 1
-def MoulinRouge():
-    
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
 
-    if PA >= 3:
-        
-        DSorts += (28+32)/2
-        BuffLanceIncendiaireTotal += BuffLanceIncendiaire
-    PA -= 3
-    return
+def LanceAIncendie(): apply_sort([23,26,28,31], 10, 'Feu', proc=True)
+def MoulinRouge(): apply_sort([28,32,34,38], 15, 'Feu', BuffLanceIncendiaire)
+def Fente(): apply_sort([12,14,16,18], 5, 'Feu', BuffLanceIncendiaire)
+def FerRouge(): apply_sort([27,30,32,36], 20, 'Feu', BuffLanceIncendiaire // 2)
+def EstocBrulant(): apply_sort([25,28,30,34], 10, 'Feu', BuffLanceIncendiaire)
+def Muspel(): apply_sort([31,35,37,42], 20, 'Feu', proc=True, muspel=True)
+def Maelstom(): apply_sort([29,32,35,38], 10, 'Feu', proc=True)
 
-# Sort Numéro 2
-def Fente():
+Sorts = [LanceAIncendie, MoulinRouge, Fente, FerRouge, EstocBrulant, Muspel, Maelstom]
+limits = [3, 2, 2, 3, 3, 2, 3]
+pa_costs = [3, 3, 2, 3, 3, 4, 3]
 
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
+def get_seq_from(pa, current_limits: list):
+    for i, (f, limit, cost) in enumerate(zip(Sorts, current_limits, pa_costs)):
+        if current_limits[i] > 1 and cost <= pa:
+            new_limits = current_limits.copy()
+            new_limits[i] -= 1
+            yield [i]
+            for subseq in get_seq_from(pa - cost, new_limits):
+                yield [i, *subseq]
 
-    if PA >= 2:
-        
-        DSorts += (12+14)/2
-        BuffLanceIncendiaireTotal += BuffLanceIncendiaire
-    PA -= 2
-    return
+def simulate_turns():
+    global Lance, DSorts, BuffLanceIncendiaireTotal, Dégats
+    tours = []
 
-# Sort Numéro 3
-def FerRouge():
+    for seq in get_seq_from(PA, limits.copy()):
+        DSorts, Dégats, BuffLanceIncendiaireTotal, Lance = initial_state
 
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
+        for idx in seq:
+            Sorts[idx]()
+        tours.append((*seq, DSorts, Dégats))
 
-    if PA >= 3:
-        
-        DSorts += (27+30)/2
-        BuffLanceIncendiaireTotal += BuffLanceIncendiaire*0.5
-    PA -= 3
-    return
+    return tours
 
-# Sort Numéro 4
-def EstocBrulant():
+tours = simulate_turns()
 
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
-
-    if PA >= 3:
-        
-        DSorts += (25+28)/2
-        BuffLanceIncendiaireTotal += BuffLanceIncendiaire
-    PA -= 3    
-    return
-
-# Sort Numéro 5
-def Muspel():
-
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
-
-    if PA >= 4:
-        
-        if Lance == True:
-            LanceIncendiaire()
-        DSorts += (31+35)/2
-    PA -= 4
-    return
-
-# Sort Numéro 6
-def Maelstom():
-
-    global Lance, DSorts, BuffLanceIncendiaire, BuffLanceIncendiaireTotal, PA
-
-    if PA >= 3:
-        
-        
-        DSorts += (16+19)/2
-        if Lance == False:
-            Lance = True
-        elif Lance == True:
-            LanceIncendiaire()
-    PA -= 3        
-    return
-
-Tour = []
-TourFiltre = []
-
-# Liste de fonctions correspondantes aux sorts feu de la Forgelance
-Sorts = (LanceAIncendie,MoulinRouge,Fente,FerRouge,EstocBrulant,Muspel,Maelstom)
-
-
-# 4 Boucle pour itérer à travers les sorts et tester toutes les combinaison possibles
-# Problèmes majeurs: 
-# Besoin de "sauvegarder" les Pa, Dégat, les Buff et l'état de la Lance à un moment donné
-# Besoin de faire une boucle par sorts lancé, serait plus pratique si ça détectait, avec les PA restants, s'il est possible de lancer un sort (utile si on se fait Re Pa)
-
-for i in range(0,len(Sorts)):
-
-    PA = 12
-    BuffLanceIncendiaireTotal = 0
-    Lance = False
-    DSorts = 0
-
-    Sorts[i]()
-
-    PAE1 = PA
-    DSortsE1 = DSorts
-    BuffLanceIncendiaireTotalE1 = BuffLanceIncendiaireTotal
-    LanceE1 = Lance
-    
-    for j in range(0,len(Sorts)):
-
-        PA = PAE1
-        BuffLanceIncendiaireTotal = BuffLanceIncendiaireTotalE1
-        Lance = LanceE1
-        DSorts = DSortsE1
-
-        Sorts[j]()
-
-        PAE2 = PA
-        DSortsE2 = DSorts
-        BuffLanceIncendiaireTotalE2 = BuffLanceIncendiaireTotal
-        LanceE2 = Lance
-
-        for k in range(0,len(Sorts)):
-
-            PA = PAE2
-            BuffLanceIncendiaireTotal = BuffLanceIncendiaireTotalE2
-            Lance = LanceE2
-            DSorts = DSortsE2
-
-            Sorts[k]()
-
-            PAE3 = PA
-            DSortsE3 = DSorts
-            BuffLanceIncendiaireTotalE3 = BuffLanceIncendiaireTotal
-            LanceE3 = Lance
-
-            if PA>0:
-                for l in range(0,len(Sorts)):
-
-                    PA = PAE3
-                    BuffLanceIncendiaireTotal = BuffLanceIncendiaireTotalE3
-                    Lance = LanceE3
-                    DSorts = DSortsE3
-                    
-                    Sorts[l]()
-
-                    if PA>=0:
-                        Tour.append((i,j,k,l,DSorts))
-                    else:
-                        Tour.append((i,j,k,DSortsE3))
-
-            elif PA == 0:
-
-                Tour.append((i,j,k,DSorts))
-
-# Sert a retirer les séquences de sorts où un sort a plus d'utilisation que sa limite par tour
-for sublist in Tour:
-    NBLAI = sublist.count(0)
-    NBMORO = sublist.count(1)
-    NBFENT = sublist.count(2)
-    NBFERO = sublist.count(3)
-    NBESTC = sublist.count(4)
-    NBMUS = sublist.count(5)
-    NBMAEL = sublist.count(6)
-    if NBLAI < 3 and NBFERO < 3 and NBESTC < 3 and NBMAEL < 3 and NBMORO < 2 and NBFENT < 2 and NBMUS < 2:
-        TourFiltre.append(sublist)
-
-# Détermine le maximum de dégats de sorts dans les conditions données
-GrosDégat = max_value(TourFiltre)
-
-# Print les séquences obtenant ce résultat
-for sublist in TourFiltre:
-    if GrosDégat in sublist:
-        print(sublist)
+tours.sort(key=lambda x:-x[-1])
+print(*tours[:5], sep='\n')
